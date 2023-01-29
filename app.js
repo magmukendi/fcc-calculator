f81c412a1eb6b3d98aa8638e28d9d94b615eda99
LeGrandMAG <magmukendi0@gmail.com>
2023-01-29 18:12:22 +0900

    completed the Calculator project
    - Built with react
    - Implemented all the requirement following the users story

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ const NUMBERS = [
 ];
 let endsWithOperator = /[+*-\/]$/;
 let opp = /\s[(+\*-\/|$]\s*/;
-let isOperator = /[+*-\/]/;
-function B(props) {
+let isOperator = /[+*-\/]+/;
+let moreOp = /[+*-\/]{2,}/;
+
+function Digit(props) {
+  
+  // This is the component to render each digits!
+  
   return (
     <button onClick={props.input} className="number" id={props.id}>
       <p>{props.digit}</p>
@@ -21,113 +26,161 @@ function B(props) {
   );
 }
 
-function O(props) {
+function Operator(props) {
+  
+  //This is a component to render the operators
   return (
     <button onClick={props.op} className="operator" id={props.name}>
-      <p>{props.sign}</p>
+      {props.sign}
     </button>
   );
 }
 
 function Love() {
+ 
+  // A state that take care of the input displayed on the calculator
   const [display, setDisplay] = React.useState(0);
+ 
+     React.useEffect(() => {
+    console.log("Input changed to: " + display);
+  },);
+  
+ 
+  
+  
 
   function input(d) {
-    if (display === 0) {
+    
+    /// A function that update the input
+    if (display === 0 || display === "0") {
+      //If the input is zero replay it with the inputed number
       setDisplay(d);
-    } else {
-      setDisplay((prev) => `${prev}${d}`);
+    } 
+    
+    else if(display.toString()[0]!== 0 || display.toString()[0]!== "0") {
+      setDisplay((prev) => {
+        let result = prev.toString()
+
+        if(result[result.length - 1].match(/[0-9|.]+/))
+          
+          // If the input number is a digit or "." 
+          {
+            console.log(result.[result.length-1] + "hey")
+            return (result !== 0 || result !== "0") && `${result}${d}`
+          }
+        
+        else{
+          
+          // Add a space if the input is not a digit
+        return (result !== 0 || result !== "0") && `${result} ${d}`;
+        }})
     }
   }
   function clear() {
+    //A  function to clear the display and return it to zero
     setDisplay(0);
   }
 
   function handleOp(e) {
-    const operator = e.target.textContent;
-    setDisplay(() => {
-      if (/[+*-\/]/g.test(display)) {
-        let fe = display.split(" ");
-        let x = fe[fe.length - 1];
+    
+    // A function that handles the operators
+    
+      /// First check if the input has already operators
+    if (/[+*-\/]/g.test(display)) {
+      let res = display.toString()
+      
+        if(isOperator.test(res[res.length - 1]))
+          {
+            
+            setDisplay(prev => {
+              
+              let good = [...prev]
+            
+              // Check if the input operator is not minus
+              if(!e.match(/-/)){
+                // Check if there is already an operator before minus, removes it.
+                if(good[good.length - 2].match(isOperator))
+                  {
 
-        if (x === "+" || x === "-" || x === "/" || x === "*") {
-          x = operator;
-          fe = fe.split("");
-          fe[fe.length - 1] = x;
-          console.log(fe);
-          return fe.join("");
-        } else {
-          return fe.join("") + " " + operator + " ";
-        }
-      } else {
-        let fe = display.toString();
-        let x = fe[fe.length - 1];
+                    good = good.slice(0,good.length - 1)
+                  }
+                
+                good[good.length - 1] = e
+              }
+              
+              
+              else if(e.match(/[-]+/)){
+                
+                // If the operator is minus, add it to the input without removing the previous operator
+              good.push(e)
+              }
 
-        console.log("val" + x);
-        if (x === "+" || x === "-" || x === "/" || x === "*") {
-          x = operator;
-          fe = fe.split("");
-          fe[fe.length - 1] = x;
-          console.log(fe);
-          return fe.join("");
-        } else {
-          return fe + " " + operator + " ";
+              good = good.join("")   
+              return good
+            })
+
+            
+          }
+        else{
+          setDisplay(prev => {
+          
+          return prev + " " + e
+        })
         }
       }
-    });
+      else{
+        setDisplay(prev => {
+          
+          return prev + " " + e
+        })
+      }
   }
 
   function handleResult() {
+    
+    // A function that displays the result
     setDisplay((prev) => {
-      let r = prev;
+      
+      if(prev.toString()[0] === 0 || prev.toString()[0] === "0")
+        {
+          console.log("first number is " +[...prev])
+          prev = prev.toString().substring(1,prev.length)
+          console.log(prev)
+        }
       return eval(prev);
     });
   }
 
-  React.useEffect(() => {
-    //console.log(display);
-  }, [display]);
+
 
   function virgule(x) {
+    
+    // A function to add the comma
     let e = [];
-    let t = x.split(" ");
+    let t = x.toLocaleString().split(" ");
     if (!t[t.length - 1].includes(".")) {
-      e = Array.from(x);
-      e.push(".");
-
-      return e.join("");
-    } else if (x.toString().includes(".") || x.toString().includes(opp)) {
-      x = x.join("").split(" ");
-      if (!x[x.length - 1].includes(".")) {
-        x[x.length - 1] = x[x.length - 1] + ".";
-      }
-      console.log(x);
-      return [...x].join(" ");
+      return x.toString() + "."
+    } 
+    else if (t[t.length - 1].includes(".")) {
+      //x = x.join("").split(" ");
+      
+      return x.toLocaleString()
+      
     }
   }
 
   function decimal() {
+    
+    // A function to proceed with decimal operation
     setDisplay((prev) => {
-      if (endsWithOperator.test(prev) === true) {
-        let re = prev.split(isOperator);
-        let number = [...prev];
-        let t = [];
-        let st = [];
-
-        console.log("Lol" + t);
-        //let div = prev.split(/\d+/g)
-        re[re.length - 1] = virgule(re[re.length - 1]);
-        return re.join("");
-      } else {
-        return virgule([...prev.toString()]);
+        return virgule(prev);
         /*console.log("prev" + [...prev]);
          */
-      }
     });
   }
 
   const NumberElements = NUMBERS.map((item) => (
-    <B input={() => input(item.d)} id={item.c} digit={item.d} />
+    <Digit input={() => input(item.d)} id={item.c} digit={item.d} />
   ));
   return (
     <div className="container">
@@ -140,17 +193,17 @@ function Love() {
           {NumberElements}
 
           <button onClick={decimal} className="number" id="decimal">
-            DEC
+            .
           </button>
           <button className="number" onClick={clear} id="clear">
             AC
           </button>
         </div>
         <div className="operator-container">
-          <O op={handleOp} sign="+" name="add" />
-          <O op={handleOp} sign="-" name="subtract" />
-          <O op={handleOp} sign="*" name="multiply" />
-          <O op={handleOp} sign="/" name="divide" />
+          <Operator op={()=>handleOp("+")} sign="+" name="add" />
+          <Operator op={()=>handleOp("-")} sign="-" name="subtract" />
+          <Operator op={()=>handleOp("*")} sign="*" name="multiply" />
+          <Operator op={()=>handleOp("/")} sign="/" name="divide" />
           <button onClick={handleResult} id="equals">
             =
           </button>
